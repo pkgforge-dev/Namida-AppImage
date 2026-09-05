e36c88a2c94b8ccb03e8200912a258de32a6f076
Muhtasham Nawr al-Mahmud <muhtaseem2005@gmail.com>
2026-09-05 21:03:09 +0600

    fix wpewebkit

--- make-appimage.sh
+++ make-appimage.sh
@@ -11,7 +11,18 @@ export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}
 export ICON=./AppDir/share/icons/namida_256.png
 export DESKTOP=./AppDir/share/applications/namida.desktop
 
-export PATH_MAPPING='/usr/lib/wpe-webkit-2.0:${SHARUN_DIR}/lib/wpe-webkit-2.0'
+export PATH_MAPPING='
+/usr/lib/wpe-webkit-2.0/WPENetworkProcess:${SHARUN_DIR}/bin/WPENetworkProcess
+/usr/lib/wpe-webkit-2.0/WPEWebProcess:${SHARUN_DIR}/bin/WPEWebProcess
+/usr/lib/wpe-webkit-2.0/WPEGPUProcess:${SHARUN_DIR}/bin/WPEGPUProcess
+/usr/lib/wpe-webkit-2.0/injected-bundle/libWPEInjectedBundle.so:${SHARUN_DIR}/lib/libWPEInjectedBundle.so
+'
 
-quick-sharun ./AppDir/namida ./AppDir/bin/* /usr/lib/wpe-webkit-2.0
+quick-sharun \
+	./AppDir/namida \
+	./AppDir/bin/* \
+	/usr/lib/wpe-webkit-2.0/WPENetworkProcess \
+	/usr/lib/wpe-webkit-2.0/WPEWebProcess \
+	/usr/lib/wpe-webkit-2.0/WPEGPUProcess \
+	/usr/lib/wpe-webkit-2.0/injected-bundle/libWPEInjectedBundle.so
 quick-sharun --make-appimage
