2299fae83f2f313f4efefbb41e0e2f89fd91b7e3
Muhtasham Nawr al-Mahmud <muhtaseem2005@gmail.com>
2026-09-05 22:07:45 +0600

    fix wpewebkit

--- make-appimage.sh
+++ make-appimage.sh
@@ -11,5 +11,12 @@ export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}
 export ICON=./AppDir/share/icons/namida_256.png
 export DESKTOP=./AppDir/share/applications/namida.desktop
 
+export PATH_MAPPING='
+/usr/lib/wpe-webkit-2.0/WPENetworkProcess:${SHARUN_DIR}/lib/WPENetworkProcess
+/usr/lib/wpe-webkit-2.0/WPEWebProcess:${SHARUN_DIR}/lib/WPEWebProcess
+/usr/lib/wpe-webkit-2.0/WPEGPUProcess:${SHARUN_DIR}/lib/WPEGPUProcess
+/usr/lib/wpe-webkit-2.0/injected-bundle/libWPEInjectedBundle.so:${SHARUN_DIR}/lib/libWPEInjectedBundle.so
+'
+
 quick-sharun ./AppDir/namida ./AppDir/bin/*
 quick-sharun --make-appimage
