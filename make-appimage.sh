818ae2d86f04cc001f475e38a8defcdc923db4c6
Muhtasham Nawr al-Mahmud <muhtaseem2005@gmail.com>
2026-09-06 13:28:19 +0600

    fix wpewebkit

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,11 +10,6 @@ export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=./AppDir/share/icons/namida_256.png
 export DESKTOP=./AppDir/share/applications/namida.desktop
-export PATH_MAPPING='
-/usr/lib/wpe-webkit-2.0/WPENetworkProcess:${SHARUN_DIR}/bin/WPENetworkProcess
-/usr/lib/wpe-webkit-2.0/WPEWebProcess:${SHARUN_DIR}/bin/WPEWebProcess
-/usr/lib/wpe-webkit-2.0/WPEGPUProcess:${SHARUN_DIR}/bin/WPEGPUProcess
-'
 
-quick-sharun ./AppDir/namida ./AppDir/bin/* /usr/lib/wpe-webkit-2.0/WPE*Process /usr/lib/wpe-webkit-2.0/injected-bundle/libWPEInjectedBundle.so
+quick-sharun ./AppDir/namida ./AppDir/bin/* /usr/lib/wpe-webkit-2.0/WPE*Process /usr/lib/wpe-webkit-2.0/injected-bundle/libWPEInjectedBundle.so /usr/share/wpe-webkit-2.0 /usr/bin/bwrap /usr/bin/xdg-dbus-proxy
 quick-sharun --make-appimage
