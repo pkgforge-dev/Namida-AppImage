95b43bb138397eedd5e495a3851f4d73c818d39d
Muhtasham Nawr al-Mahmud <muhtaseem2005@gmail.com>
2026-09-06 15:01:18 +0600

    fix wpewebkit

--- make-appimage.sh
+++ make-appimage.sh
@@ -5,7 +5,7 @@ set -eu
 ARCH=$(uname -m)
 export ARCH
 export OUTPATH=./dist
-export MAIN_BIN=./AppDir/namida
+export MAIN_BIN=./AppDir/bin/namida
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=./AppDir/bin/share/icons/namida_256.png
