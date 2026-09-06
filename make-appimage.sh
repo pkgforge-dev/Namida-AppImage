6b3eff59d10bff5c8885706692d9075ba202aeed
Muhtasham Nawr al-Mahmud <muhtaseem2005@gmail.com>
2026-09-06 15:39:29 +0600

    fix wpewebkit

--- make-appimage.sh
+++ make-appimage.sh
@@ -11,7 +11,8 @@ export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}
 export ICON=./AppDir/bin/share/icons/namida_256.png
 export DESKTOP=./AppDir/bin/share/applications/namida.desktop
 
+# FIXME: Temporary measure till quick-sharun adds support for wpewebkit sandboxing
 echo "WEBKIT_DISABLE_SANDBOX_THIS_IS_DANGEROUS=1" >> ./AppDir/.env
 
-quick-sharun ./AppDir/bin/* /usr/lib/wpe-webkit-2.0
+quick-sharun ./AppDir/bin/* /usr/lib/libmpv.so* /usr/lib/wpe-webkit-2.0
 quick-sharun --make-appimage
