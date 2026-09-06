f243338884ae1b7529d76113f23f327ecefe308b
Muhtasham Nawr al-Mahmud <muhtaseem2005@gmail.com>
2026-09-06 14:56:21 +0600

    fix wpewebkit

--- make-appimage.sh
+++ make-appimage.sh
@@ -11,15 +11,10 @@ export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}
 export ICON=./AppDir/share/icons/namida_256.png
 export DESKTOP=./AppDir/share/applications/namida.desktop
 
-echo "WEBKIT_DISABLE_SANDBOX_THIS_IS_DANGEROUS=1" >> ./AppDir/.env
-
 quick-sharun \
-    ./AppDir/namida \
-    ./AppDir/bin/* \
-    /usr/lib/libWPEBackend-fdo-1.0.so.1 \
+    ./AppDir/* \
     /usr/lib/wpe-webkit-2.0 \
-    /usr/lib/wpe-webkit-2.0/WPE*Process \
-    /usr/share/wpe-webkit-2.0 \
+    /usr/bin/mpv \
     /usr/bin/bwrap \
     /usr/bin/xdg-dbus-proxy
 quick-sharun --make-appimage
